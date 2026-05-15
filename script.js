29d490e53cfc8f8c2fcbb166c21b4fb7a97b6055
R4D <maduranga0117@gmail.com>
2026-05-15 08:44:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function showOffer() {
   const offer = "Buy 1 Latte, Get 1 Free — Today Only!";
   document.getElementById("offerText").innerText = offer;
 }
+
+// ගීත පාලනය කරන ක්‍රියාවන් (Functions)
+var myAudio = document.getElementById("myMusicPlayer");
+
+function playMusic() {
+    if (myAudio) {
+        myAudio.play();
+    }
+}
+
+function pauseMusic() {
+    if (myAudio) {
+        myAudio.pause();
+    }
+}
+
+function stopMusic() {
+    if (myAudio) {
+        myAudio.pause();
+        myAudio.currentTime = 0; // සින්දුව මුලටම ගෙන යාම
+    }
+}
