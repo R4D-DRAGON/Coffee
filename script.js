361652cdde7157d1af223eaf20368599f1a0127c
R4D <maduranga0117@gmail.com>
2026-05-15 13:24:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,3 @@ function showOffer() {
   document.getElementById("offerText").innerText = offer;
 }
 
-function playMusic() {
-    var myAudio = document.getElementById("myMusicPlayer");
-    if (myAudio) { myAudio.play(); }
-}
-
-function pauseMusic() {
-    var myAudio = document.getElementById("myMusicPlayer");
-    if (myAudio) { myAudio.pause(); }
-}
-
-function stopMusic() {
-    var myAudio = document.getElementById("myMusicPlayer");
-    if (myAudio) {
-        myAudio.pause();
-        myAudio.currentTime = 0;
-    }
-}
-    var myAudio = document.getElementById("myMusicPlayer");
